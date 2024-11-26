fc4a8a9be141a3376959c959fc6d508e8e86dd80
bruno martinez <brumarti@umag.cl>
2024-11-26 16:08:34 -0300

    estructura para comentarios de un archivo

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
-#include "utilities.h"
-#include "errors.h"
-#include "hash.h"
+#include <stdlib.h>
+#include "comment.h"
 
-int main() {
-    printf("Bienvenido al programa LoopWeb\n");
+int main()
+{
+    // PRUEBA DE COMENTARIOS
+    CommentList comentarios = NULL;
+    comentarios = read_comments_from_file(comentarios, "./build/com.txt");
+    if(comentarios == NULL){
+        printf("Error al crear la lista de comentarios\n");
+        exit(1);
+    }
+    printf("\nComentarios leidos\n");
+    
+    print_comments(comentarios);
+
+    delete_comment_list(comentarios);
+    // FIN PRUEBA DE COMENTARIOS
 
     return 0;
 }
